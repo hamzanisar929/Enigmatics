6678d342905e89fcaa6f93f4a3bb6face135acb2
Asad-960 <asadullahafzaal123@gmail.com>
2025-02-07 14:43:24 +0500

    Added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import enigmatics from "../assets/enigmatics.svg";
 
 const Header = () => {
   const pathname = useLocation();
@@ -36,7 +37,7 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          {/* <img src={enigmatics} width={190} height={40} alt="Enigmatics" /> Gonna put logo here Asad  */}
+          <img src={enigmatics} width={190} height={40} alt="Enigmatics" />
         </a>
 
         <nav
